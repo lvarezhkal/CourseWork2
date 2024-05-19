c2fb5fc105328108db66faa8167db4ff59b44bde
lvarezhkal <kkakxaa@gmail.com>
2024-05-19 16:14:13 +0300

    start

---
+++ src/main/java/pro/sky/exam/ExamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamApplication.class, args);
+	}
+
+}
